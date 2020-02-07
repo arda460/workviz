94fe007e90e2c2ef2da985b195305f3014ddb3c5
Alexander <fight4real@hotmail.com>
2020-02-07 18:32:55 +0100

    BarChart init

---
+++ src/components/BarChart/BarChart.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import * as d3 from 'd3'
+
+
+
+const margin = {top:20,left:40,right:30,bottom: 30}
+const height = 500
+const width = 500
+
+
+const dummyData = {
+  foo:2,bar:4,banana:3,teacup:10
+}
+
+
+function BarChart() {
+  return (
+    <div>
+      this will be a barchart
+    </div>
+  )
+}
+
+
+
+export default BarChart
