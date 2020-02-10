0f76abfa83ee482a4fafd26b91eaa8bd67bdc04e
Alexander <fight4real@hotmail.com>
2020-02-10 14:06:14 +0100

    moved out props from component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import './App.css';
-import BarChart from './components/BarChart/BarChart'
+import React from "react";
+import "./App.css";
+import BarChart from "./components/BarChart/BarChart";
+
+//Set properties for the barchart
+const margin = {
+  top: 20,
+  right: 10,
+  bottom: 20,
+  left: 10
+};
+const height = 500;
+const width = 500;
+
+//super simple data, normally this would be much more complicated
+const dummyData = [
+  {
+    name: "apple",
+    value: 3
+  },
+  {
+    name: "pen",
+    value: 2
+  },
+  {
+    name: "banana",
+    value: 7
+  },
+  {
+    name: "apple-pen",
+    value: 5
+  },
+  {
+    name: "superbanana",
+    value: 11
+  }
+];
+
+let props = {
+  margin,
+  height,
+  width,
+  data: dummyData
+};
+
 function App() {
   return (
     <div className="App">
-      <BarChart/>
+      <BarChart {...props} />
     </div>
   );
 }
--- src/components/BarChart/BarChart.js
+++ src/components/BarChart/BarChart.js
@@ -1,44 +1,13 @@
 import React, { useRef, useEffect } from "react";
 import * as d3 from "d3";
 
-const margin = {
-  top: 20,
-  right: 10,
-  bottom: 20,
-  left: 10
-};
-const height = 500;
-const width = 500;
-
-//super simple data, normally this will be much more complicated
-const dummyData = [
-  {
-    name: "apple",
-    value: 3
-  },
-  {
-    name: "pen",
-    value: 2
-  },
-  {
-    name: "banana",
-    value: 7
-  },
-  {
-    name: "pen-apple apple-pen",
-    value: 5
-  },
-  {
-    name: "superbanana",
-    value: 11
-  }
-];
-//all values above should come from props in BarChart...
-
 function BarChart(props) {
+  const { height, width, margin, data } = props;
+
   //create a svg reference for d3 to prevent conflicts
   let svgRef = useRef(null);
 
+  //draw the chart
   const draw = () => {
     const svg = d3
       .select(svgRef.current)
@@ -51,44 +20,45 @@ function BarChart(props) {
 
     const x = d3
       .scaleBand()
-      .range([0, width - margin.left - margin.right])
-      .domain(dummyData.map(x => x.name))
+      .range([0, width - margin.left - margin.right]) //range is the pixelsize of the svg element for said axis
+      .domain(data.map(x => x.name)) //domain is the range of values
       .padding(0.1);
 
     const y = d3
       .scaleLinear()
-      .range([height - margin.top - margin.bottom, 0]) //range is the pixelsize of the graph
-      .domain([0, d3.max(dummyData.map(fruit => fruit.value))]); //domain is the range of values
+      .range([height - margin.top - margin.bottom, 0])
+      .domain([0, d3.max(data.map(fruit => fruit.value))]);
 
     //prettier-ignore
     const bars = g
       .selectAll("UltimateBarChart")
-      .data(dummyData)
+      .data(data)
       .join("rect")
       .attr("x", d => {return x(d.name)})
-      .attr("y", d=> {return y(d.value)-40})
+      .attr("y", d => {return y(d.value) - 40})
       .attr("width", x.bandwidth())
-      .attr("height", d => {return (height  - y(d.value))})
+      .attr("height", d => {return (height - y(d.value))})
       .attr("fill","cadetblue")
 
-    let xAxis = d3.axisBottom(x);
+    const xAxis = d3.axisBottom(x);
 
     svg
       .append("g")
       .attr(
         "transform",
-        "translate(" + 0 + "," + (height - margin.bottom) + ")"
+        "translate(" + margin.left + "," + (height - margin.bottom) + ")"
       )
       .call(xAxis);
   };
 
+  //React magic!
   useEffect(() => {
     draw();
   });
 
   return (
     <div>
-      <p>this will be a barchart </p>
+      <h3>BarChart</h3>
       <svg ref={svgRef} width={width} height={height}></svg>
     </div>
   );
