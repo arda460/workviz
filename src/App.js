d4b70674acf7128e48603849308a0adbed472f3e
Alexander <fight4real@hotmail.com>
2020-02-18 18:38:09 +0100

    added line and small bars bigger and greener
    seperated fetching and rendering
    
    moved some styling to css
    
    cleaned up some code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import BarChart from "./components/BarChart/BarChart";
+import BarChartData from "./components/BarChart/BarChartData";
 
 function App() {
   const [barHover, setBarHover] = useState("hover over bar to see employee");
 
+  const updatebarHover = check => {
+    if (typeof check.value != "undefined") {
+      setBarHover(check);
+    }
+  };
+
   return (
     <div className="App">
       <h3>WorkVis</h3>
-      <BarChart selectPerson={setBarHover} />
-      {barHover}
+      <BarChartData selectPerson={updatebarHover} />
+      {`${barHover.name}, ${barHover.value} %`}
     </div>
   );
 }
