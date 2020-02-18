44e6e4cac99d31794c7a432d20150616e216992a
Alexander <fight4real@hotmail.com>
2020-02-18 14:24:23 +0100

    commented out firebase(config)

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import * as firebase from 'firebase';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import * as firebase from "firebase";
 
 //Firebase Configuration Below
-  var firebaseConfig = {
-    apiKey: "AIzaSyAvpDPP3ObMeXohV8Fix-YE0Bs8MEISh_o",
-    authDomain: "workwiz-2aee8.firebaseapp.com",
-    databaseURL: "https://workwiz-2aee8.firebaseio.com",
-    projectId: "workwiz-2aee8",
-    storageBucket: "workwiz-2aee8.appspot.com",
-    messagingSenderId: "4359964425",
-    appId: "1:4359964425:web:30f29d38fd4984e3d035d8",
-    measurementId: "G-BHWHPMG7LZ"
-  };
+var firebaseConfig = {
+  apiKey: "AIzaSyAvpDPP3ObMeXohV8Fix-YE0Bs8MEISh_o",
+  authDomain: "workwiz-2aee8.firebaseapp.com",
+  databaseURL: "https://workwiz-2aee8.firebaseio.com",
+  projectId: "workwiz-2aee8",
+  storageBucket: "workwiz-2aee8.appspot.com",
+  messagingSenderId: "4359964425",
+  appId: "1:4359964425:web:30f29d38fd4984e3d035d8",
+  measurementId: "G-BHWHPMG7LZ"
+};
 
 //Initialize Firebase
-firebase.initializeApp(config);
+//firebase.initializeApp(config);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
