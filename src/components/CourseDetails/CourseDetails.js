966291b1ecde613a6b9c7d0b1d9cc5aa6dba0fd1
arnar44 <arnar44@gmail.com>
2020-02-25 17:41:34 +0100

    course detail layout

--- src/components/CourseDetails/CourseDetails.js
+++ src/components/CourseDetails/CourseDetails.js
@@ -6,40 +6,27 @@ export default function CourseDetails(props) {
     const { courseKey, data } = props;
     const d = useRef(null);
 
-    if (courseKey) {
-        console.log(data);
+    if (courseKey) 
         d.current = data['autumnData'][courseKey] ? data['autumnData'][courseKey] : data['springData'][courseKey];
-    }
+    
+
+    console.log(d);
 
     return (
-        <div className="courseDetails">
+        <div className="courseFlex">
             {d.current &&
-            <div className="detailsItem">
-                <h4>Teachers</h4>
-                {
-                Object.keys(d.current['Teachers']).map(teach => {
-                    return (
-                        <ul key={teach}>
-                            <li key={teach}>{teach}</li>
-                        </ul>
-                    )
-                })
-                }
-            </div>
+                <div className="detailsCol">
+                    <div className="detailsRow"></div>
+                    <div className="detailsRow"></div>
+                    <div className="detailsRow"></div>
+                    <div className="detailsRow"></div>
+                </div>
             }
             {d.current &&
-            <div className="detailsItem">
-                <h4>Periods</h4>
-                {
-                d.current['Period'].map(teach => {
-                    return (
-                        <ul key={teach}>
-                            <li key={teach}>{teach}</li>
-                        </ul>
-                    )
-                })
-                }
-            </div>
+                <div className="detailsCol">
+                    <div className="detailsRow"></div>
+                    <div className="detailsRow"></div>
+                </div>
             }
         </div>
     )
