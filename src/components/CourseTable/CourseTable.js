9e3372ef369bdd364279efb9df33af28fcb1ce45
arnar44 <arnar44@gmail.com>
2020-02-26 16:54:17 +0100

    course overview responsive on teacher hover

--- src/components/CourseTable/CourseTable.js
+++ src/components/CourseTable/CourseTable.js
@@ -1,17 +1,12 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import CourseTableContent from "../CourseTableContent/CourseTableContent";
 import CourseDetails from "../CourseDetails/CourseDetails";
-import { GlobalStateContext } from "../../context/GlobalStateContext";
-import { DataContext } from "../../context/DataContext";
 
 import "./CourseTable.css";
 
 export default function CourseTable(props) {
   const [data, setData] = useState(null);
   const [courseDetails, setCourseDetails] = useState(false);
-
-  const { personHover } = useContext(GlobalStateContext); //get related value from  the global state
-  const { summary20, loading } = useContext(DataContext); //get summary20Data which has the course data for each person
 
   useEffect(() => {
     async function getData() {
@@ -37,16 +32,6 @@ export default function CourseTable(props) {
           data={data}
         ></CourseTableContent>
       </div>
-
-      <p></p>
-      <p>
-        {!loading && personHover != null
-          ? summary20[personHover.name]["VT Courses"].map(
-              c => ` ${c["Course Code"]}`
-            )
-          : ""}
-      </p>
-
       <div className="courseDetailsContainer">
         <CourseDetails courseKey={courseDetails} data={data}></CourseDetails>
       </div>
--- src/components/Period/Period.js
+++ src/components/Period/Period.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, {useContext} from "react";
 import "./Period.css";
+import { GlobalStateContext } from "../../context/GlobalStateContext";
+import { DataContext } from "../../context/DataContext";
 
 export default function Period(props) {
     const { toMap, classN, colors, over, setHover, setCourseDetails } = props;
+    const { personHover } = useContext(GlobalStateContext); //get related value from  the global state
+    const { summary20, loading } = useContext(DataContext); //get summary20Data which has the course data for each person
+
+
     return (
         <div className={classN}>
             {toMap &&
                 toMap.map(course => {
                     const color = colors[course];
                     const c1 = color ? `button ${color}` : 'button'; 
-                    const c2 = over === course ? ' hover' : ''; 
-                    const cName = c1 + c2;
+                    // If hovering on course overView
+                    const c2 = over === course ? ' hover' : '';
+
+                    let c3 = '';
+                    // If hovering on techer overView
+                    if (!loading && personHover) {
+                        const vt = summary20[personHover.name]["VT Courses"].filter( c => course === c['Course Code']);
+                        const ht = summary20[personHover.name]["HT Courses"].filter( c => course === c['Course Code']);
+                        c3 = vt.length > 0 || ht.length > 0 ? ' hover' : '';
+                    }
+                    const c4 = c3 ? c3 : c2;
+                    const cName = c1 + c4;
                     return (
                         <button key={course} className={cName} data-id={course} onClick={ e => setCourseDetails(e.currentTarget.dataset.id)} onMouseEnter={() => setHover(course)} onMouseLeave={() => setHover('')}>{course}</button>
                     )
