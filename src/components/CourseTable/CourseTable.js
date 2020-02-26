d35adc7ae2447ced1907244cf56f581b493dde0c
arnar44 <arnar44@gmail.com>
2020-02-26 17:40:49 +0100

    Refactor CourseTable.js to use context data

--- src/components/CourseTable/CourseTable.js
+++ src/components/CourseTable/CourseTable.js
@@ -1,27 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import CourseTableContent from "../CourseTableContent/CourseTableContent";
 import CourseDetails from "../CourseDetails/CourseDetails";
+import { DataContext } from "../../context/DataContext";
 
 import "./CourseTable.css";
 
 export default function CourseTable(props) {
   const [data, setData] = useState(null);
   const [courseDetails, setCourseDetails] = useState(false);
+  const { HT20, VT20, loading } = useContext(DataContext);
 
   useEffect(() => {
-    async function getData() {
-      const autumnResponse = await fetch("./Data/HT20.json");
-      const autumnData = await autumnResponse.json();
-      const springResponse = await fetch("./Data/VT20.json");
-      const springData = await springResponse.json();
-      const data = {
-        autumnData: autumnData,
-        springData: springData
+    if (!loading){
+      const d = {
+        autumnData: HT20,
+        springData: VT20
       };
-      setData(data);
-    }
-    getData();
-  }, []);
+      setData(d);
+    } 
+  }, [HT20, VT20, loading]);
 
   return (
     <div className="courseContainer">
