91bad9b6af2123c6a281b6b45d8cbf6d7253f189
arnar44 <32617343+arnar44@users.noreply.github.com>
2020-02-26 16:13:18 +0100

    Added course responsibles in summary and HT/VT, changed string to numb in summary (procentages) and set all course codes to upp case in summary (#11)

--- dataScripts/summaryScript.py
+++ dataScripts/summaryScript.py
@@ -1,13 +1,27 @@
 import csv
 import json
 import pandas as pd
+import re
 
 # Global
 filename = "./rawData/summary20.csv"
 outputName = "summary20.json"
- 
-def getCourses(row, k):
-    courseList = []
+
+def cleanNumb(numb):
+    # Change dash to minus
+    numbNoDash = numb.replace('−','-')
+    # Change , into .
+    numbNoComma = numbNoDash.replace(',', '.')
+    # remove \r endings
+    numbNoEscape = numbNoComma if not re.search(r'\r', numbNoComma) else numbNoComma[:-1]
+    # Remove % sign from end
+    numbNoProc = numbNoEscape[:-1] if numbNoEscape.endswith('%') else numbNoEscape
+    # If only . then replace with zero
+    numbNoEmpty = 0 if numbNoProc == '.' else numbNoProc
+    return float(numbNoEmpty)
+
+def getResponsible(row, k):
+    responsible = []
     # Handle multible courses in column
     if not isinstance(row[k], float):
         # Get all course lines
@@ -15,10 +29,28 @@ def getCourses(row, k):
         for i in range(len(lines)):
             # Split course line into code,name and %
             course = lines[i].split(' ')
+            
+            if course[0][0] == '*':
+                responsible.append(course[0][1:].upper())            
+    
+    return responsible
+ 
+def getCourses(row, k):
+    courseList = []
+    
+    # Handle multible courses in column
+    if not isinstance(row[k], float):
+        # Get all course lines
+        lines = row[k].split('\n')
+        for i in range(len(lines)):
+            # Split course line into code,name and %
+            course = lines[i].split(' ')
+            code = course[0][1:] if course[0][0] == '*' else course[0]
+
             tmp = {
-                "Course Code": course[0],
+                "Course Code": code.upper(),
                 "Short Name": course[1][:-1], # Remove ":" from name
-                "Work %": course[2]
+                "Work %": cleanNumb(course[2])
             }
             courseList.append(tmp)
     return courseList
@@ -38,21 +70,39 @@ def main():
 
             teachers[row["Name"]]["Position"] = clean(row, "Position", "NA")            
             teachers[row["Name"]]["Department"] = clean(row, "Dept", "NA")
-            teachers[row["Name"]]["Kontering HCT (%)"] = clean(row, "Kontering HCT (%)", "0")
-            teachers[row["Name"]]["Bemnnad HCT Gru (%)"] = clean(row, "Bemnnad HCT Gru (%)", "0")
-    
-            teachers[row["Name"]]["VT Courses"] = getCourses(row, "VT courses + exjobb")
-            teachers[row["Name"]]["Gru VT (%)"] = row["Gru VT (%).1"]
-    
-            teachers[row["Name"]]["HT Courses"] = getCourses(row, "HT courses")
-            teachers[row["Name"]]["Gru HT (%)"] = row["Gru HT (%).1"]
 
-            teachers[row["Name"]]["Self-development &Friskvård (%)"] = clean(row, "Self-development &Friskvård (%)", "0")
-            teachers[row["Name"]]["Extra (%)"] = clean(row, "Extra (%)", "0")
-            teachers[row["Name"]]["Balance (%)"] = clean(row, "Balance (%)", "0")
-            teachers[row["Name"]]["GRU balance 19"] = clean(row, "GRU balance 19", "0")
-            teachers[row["Name"]]["GRU balance 20"] = clean(row, "GRU balance 20", "0")
-            teachers[row["Name"]]["Årsarbetstid %"] = clean(row, "Årsarbetstid %", "0")
+            tmp = clean(row, "Kontering HCT (%)", "0")
+            teachers[row["Name"]]["Kontering HCT (%)"] = cleanNumb(tmp)
+
+            tmp = clean(row, "Bemnnad HCT Gru (%)", "0")
+            teachers[row["Name"]]["Bemnnad HCT Gru (%)"] = cleanNumb(tmp)
+    
+            teachers[row["Name"]]["Responsible VT"] = getResponsible(row, "VT courses + exjobb")
+            teachers[row["Name"]]["VT Courses"] = getCourses(row, "VT courses + exjobb")
+            teachers[row["Name"]]["Gru VT (%)"] = cleanNumb(row["Gru VT (%).1"])
+    
+            teachers[row["Name"]]["Responsible HT"] = getResponsible(row, "HT courses")
+            teachers[row["Name"]]["HT Courses"] = getCourses(row, "HT courses")
+            teachers[row["Name"]]["Gru HT (%)"] = cleanNumb(row["Gru HT (%).1"])
+
+            tmp = clean(row, "Self-development &Friskvård (%)", "0")
+            teachers[row["Name"]]["Self-development &Friskvård (%)"] = cleanNumb(tmp)
+
+            tmp = clean(row, "Extra (%)", "0")
+            teachers[row["Name"]]["Extra (%)"] = cleanNumb(tmp)
+
+            tmp = clean(row, "Balance (%)", "0")
+            teachers[row["Name"]]["Balance (%)"] = cleanNumb(tmp)
+
+            tmp = clean(row, "GRU balance 19", "0")
+            teachers[row["Name"]]["GRU balance 19"] = cleanNumb(tmp)
+
+            tmp = clean(row, "GRU balance 20", "0")
+            teachers[row["Name"]]["GRU balance 20"] = cleanNumb(tmp)
+
+            tmp = clean(row, "Årsarbetstid %", "0")
+            teachers[row["Name"]]["Årsarbetstid %"] = cleanNumb(tmp)
+
 
     with open(outputName, 'w', encoding='utf8') as outfile:
         json.dump(teachers, outfile, ensure_ascii=False)
