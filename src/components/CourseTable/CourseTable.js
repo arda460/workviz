82f45d1cb6336e266aaa819eca5db424e6aaafab
arnar44 <32617343+arnar44@users.noreply.github.com>
2020-02-27 23:56:01 +0100

    Filter courses (#15)
    * search functionality for course overview
    
    * Opacity change when course hover + add transition time to reduce blink

--- src/components/CourseTable/CourseTable.js
+++ src/components/CourseTable/CourseTable.js
@@ -10,17 +10,54 @@ export default function CourseTable(props) {
   const { HT20, VT20, loading } = useContext(DataContext);
 
   useEffect(() => {
-    if (!loading){
+    if (!loading) {
       const d = {
         autumnData: HT20,
         springData: VT20
       };
       setData(d);
-    } 
+    }
   }, [HT20, VT20, loading]);
+
+  const handleKeyStroke = (val) => {
+    if (data !== null) {
+      const d = {
+        autumnData: HT20,
+        springData: VT20
+      };
+
+      if (val === '') {
+        setData(d);
+      }
+      else {
+        const filterdAutumn = Object.keys(d['autumnData']).filter(key => key.includes(val.toUpperCase()))
+          .reduce((obj, key) => {
+            obj[key] = d['autumnData'][key];
+            return obj;
+          }, {});
+  
+        const filterdSpring = Object.keys(d['springData']).filter(key => key.includes(val.toUpperCase()))
+          .reduce((obj, key) => {
+            obj[key] = d['springData'][key];
+            return obj;
+          }, {});
+  
+        const filtered = {
+          autumnData: filterdAutumn,
+          springData: filterdSpring
+        };
+
+        setData(filtered);
+      }
+
+    }
+  }
 
   return (
     <div className="courseContainer">
+      <div className="courseSearch">
+        <input type="search" id="courseSeachInp" onKeyUp={e => handleKeyStroke(e.target.value)}></input>
+      </div>
       <div className="courseOverview">
         <CourseTableContent
           {...props}
