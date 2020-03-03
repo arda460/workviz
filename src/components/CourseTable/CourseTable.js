45c0f0e674a49bff7b00b2b3c05f78d4e791f68d
arnar44 <arnar44@gmail.com>
2020-03-03 12:44:31 +0100

    Add toggle button for Course/teacher search

--- src/components/CourseTable/CourseTable.js
+++ src/components/CourseTable/CourseTable.js
@@ -8,7 +8,8 @@ import "./CourseTable.css";
 export default function CourseTable(props) {
   const [data, setData] = useState(null);
   const { colorDist, HT20, VT20, loading } = useContext(DataContext);
-  const [ buttonName, setButtonName ] = useState(['filterButton', 'filterButton']);
+  const [buttonName, setButtonName] = useState(['filterButton', 'filterButton']);
+  const [ sliderStatus, setSliderStatus ] = useState(false);
 
 
   useEffect(() => {
@@ -76,7 +77,7 @@ export default function CourseTable(props) {
 
       let bNames = ['filterButton', 'filterButton']
 
-      if( (val === 'red' && buttonName[0] === 'filterButton pressed') || (val === 'orange' && buttonName[1] === 'filterButton pressed') ) {
+      if ((val === 'red' && buttonName[0] === 'filterButton pressed') || (val === 'orange' && buttonName[1] === 'filterButton pressed')) {
         setData(d);
         setButtonName(bNames);
         return;
@@ -85,7 +86,7 @@ export default function CourseTable(props) {
       bNames[0] = val === 'red' ? 'filterButton pressed' : bNames[0]
       bNames[1] = val === 'orange' ? 'filterButton pressed' : bNames[1]
 
-      const courseColors = {...colorDist[0], ...colorDist[1]};
+      const courseColors = { ...colorDist[0], ...colorDist[1] };
 
       const filterdAutumn = Object.keys(d['autumnData'])
         .filter(key => courseColors[key] === val)
@@ -101,49 +102,62 @@ export default function CourseTable(props) {
           return obj;
         }, {});
 
-        const filtered = {
-          autumnData: filterdAutumn,
-          springData: filterdSpring
-        };
+      const filtered = {
+        autumnData: filterdAutumn,
+        springData: filterdSpring
+      };
 
-        setData(filtered);
-        setButtonName(bNames)
+      setData(filtered);
+      setButtonName(bNames)
     };
   };
 
-    return (
-      <div className="courseContainer">
-        <div className="courseControls">
-          <div className="courseSearch">
-            <label className="searchLabel">Search for course code/short name</label>
+  const handleSlider = () => {
+    const val = document.getElementById('searchSlider').checked;
+    setSliderStatus(val);
+  }
+
+  return (
+    <div className="courseContainer">
+      <div className="courseControls">
+        <div className="courseSearch">
+          <label className="searchLabel">Search for course code/short name</label>
+          <div className="searchToggle">
             <input type="search" id="courseSeachInp" className="searchBar" onChange={e => handleKeyStroke(e.target.value)}></input>
-          </div>
-          <div className="courseFilter">
-            <label className="filterLabel">Filter Options</label>
-            <div>
-              <button className={buttonName[0]} onClick={() => handleFilter('red')}>Filter on Red</button>
-              <button className={buttonName[1]} onClick={() => handleFilter('orange')}>Filter on Yellow</button>
-            </div>
-          </div>
-          <div className="courseInfo">
-            <div className="redInfo">
-                <div className="redBox"></div>
-                <p className="reText">Task Hour Allocation Needed</p>
-            </div>
-            <div className="yellowInfo">
-                <div className="yellowBox"></div>
-                <p className="yellowText">Teacher Allocation Needed</p>
+            <div className="toggler">
+              <label className="switch">
+                <input type="checkbox" id="searchSlider" onClick={() => handleSlider()} />
+                <span className="slider round"></span>
+              </label>
             </div>
           </div>
         </div>
-        <div className="courseOverview">
-          <CourseTableContent
-            data={data}
-          ></CourseTableContent>
+        <div className="courseFilter">
+          <label className="filterLabel">Filter Options</label>
+          <div>
+            <button className={buttonName[0]} onClick={() => handleFilter('red')}>Filter on Red</button>
+            <button className={buttonName[1]} onClick={() => handleFilter('orange')}>Filter on Yellow</button>
+          </div>
         </div>
-        <div className="courseDetailsContainer">
-          <CourseDetails data={data}></CourseDetails>
+        <div className="courseInfo">
+          <div className="redInfo">
+            <div className="redBox"></div>
+            <p className="reText">Task Hour Allocation Needed</p>
+          </div>
+          <div className="yellowInfo">
+            <div className="yellowBox"></div>
+            <p className="yellowText">Teacher Allocation Needed</p>
+          </div>
         </div>
       </div>
-    );
-  }
+      <div className="courseOverview">
+        <CourseTableContent
+          data={data}
+        ></CourseTableContent>
+      </div>
+      <div className="courseDetailsContainer">
+        <CourseDetails data={data}></CourseDetails>
+      </div>
+    </div>
+  );
+}
